30957311a624ae5d1bba0c9671341ad2358963e7
sumonDev13 <sumonmondal1530765@gmail.com>
2023-12-01 09:39:04 +0600

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { config } from "dotenv";
+
+config();
+
+
+const MONGO_URL = process.env.MONGO_URI;
+
+if(!MONGO_URL) {
+    throw new Error("No MONGO_URL specified")
+}
+
+export const dbConnection = async()=>{
+    try {
+        await mongoose.connect(MONGO_URL);
+        console.log('Connected to Database')
+    } catch (error) {
+        console.log("Error connecting to Database")
+        process.exit(1);
+    }
+};
