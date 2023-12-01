e5418f39a81db6c42a61f39072e20102f9b6fe9f
sumonDev13 <sumonmondal1530765@gmail.com>
2023-12-01 17:40:18 +0600

    user image

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          VirtualBook
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
