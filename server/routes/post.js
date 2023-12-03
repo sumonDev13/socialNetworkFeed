ba1010d1dd78d8fad7e87bc1d71c34c8d0c7cb66
sumonDev13 <sumonmondal1530765@gmail.com>
2023-12-03 12:28:36 +0600

    post router

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,8 +1,21 @@
 import express from "express";
 import { verifyToken } from "../middleware/auth-middleware.js";
+import { createPost,getPost,getUserPosts,getTimelinePosts,updatePost,likePost,dislikePost,deletePost } from "../controllers/post-controller.js";
 
 const postRouter = express.Router();
 
-postRouter.post('/',);
+postRouter.post('/',verifyToken,createPost);
+
+postRouter.get("/find/:id", getPost)
+postRouter.get("/find/userposts/:id", getUserPosts)
+postRouter.get('/timelinePosts', verifyToken, getTimelinePosts)
+
+postRouter.put("/updatePost/:id", verifyToken, updatePost)
+postRouter.put("/likePost/:postId", verifyToken, likePost)
+postRouter.put("/dislikePost/:postId", verifyToken, dislikePost)
+
+postRouter.delete('/deletePost/:id', verifyToken, deletePost)
+
+
 
 export default postRouter;
