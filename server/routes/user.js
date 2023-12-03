de1a9e7f3bcfe172a86fe0cc6788957754336357
sumonDev13 <sumonmondal1530765@gmail.com>
2023-12-03 12:59:58 +0600

    user controller

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,18 @@
 import express from "express";
-import {
-  getUser,
-} from "../controllers/users-controller.js";
+import { getAll,getUser,getUserFriends,updateUser,followUser,unfollowUser,deleteUser } from "../controllers/user-controller.js";
 import { verifyToken } from "../middleware/auth-middleware.js";
 
-const router = express.Router();
+const userRouter = express.Router();
 
-/* READ */
-router.get("/:id", verifyToken, getUser);
+userRouter.get('/findAll', getAll)
+userRouter.get('/find/:id', getUser)
+userRouter.get('/find/userfriends/:id', getUserFriends)
+
+userRouter.put('/update/:id', verifyToken, updateUser)
+userRouter.put("/follow/:id", verifyToken, followUser)
+userRouter.put('/unfollow/:id', verifyToken, unfollowUser)
+
+userRouter.delete('/delete/:id', verifyToken, deleteUser)
 
 
-export default router;
+export default userRouter;
