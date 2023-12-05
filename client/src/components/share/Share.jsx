f3e1ada3bb2c8228d0e7ee69b9de2a7a883ebfc6
sumonDev13 <sumonmondal1530765@gmail.com>
2023-12-05 12:25:22 +0600

    sidebar

---
+++ client/src/components/share/Share.jsx
@@ -0,0 +1,9 @@
+
+
+const Share = () => {
+  return (
+    <div>Share</div>
+  )
+}
+
+export default Share
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,20 @@
+import Friends from "../../components/friends/Friends";
+import Navbar from "../../components/navbar/Navbar";
+import Posts from "../../components/posts/Posts";
+import Sidebar from "../../components/sidebar/Sidebar";
 import classes from "./home.module.css";
 
 const Home = () => {
   return (
-    <div className={classes}>
-        Home
+    <div className={classes.container}>
+       <div className={classes.wrapper}>
+        <Navbar/>
+        <main className={classes.main}>
+         <Sidebar/>
+         <Posts/>
+         <Friends/>
+        </main>
+       </div>
     </div>
   )
    
